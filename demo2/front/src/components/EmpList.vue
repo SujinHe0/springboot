7e73e1134e54ef8b923e6ec2f0329b9e8d712eca
sujin <hsujin0819@gamil.com>
2023-01-04 12:40:05 +0900

    test

---
+++ demo2/front/src/components/EmpList.vue
@@ -0,0 +1,39 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>사원번호</th>
+                <th>이름</th>
+                <th>성</th>
+                <th>직무</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="emp in empList" v-bind:key="emp.employeeId">
+                <td v-text="emp.employeeId"/>
+                <td>{{emp.firstName}}</td>
+                <td v-text="emp.lastName"/>
+                <td>{{emp.jobId}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'EmpList',
+    data: function(){
+        return {
+            empList: {}
+        }
+    },
+    created: function(){
+        fetch('http://localhost/empInfoList')
+        .then(response => response.json())
+        .then(data => {
+            this.empList = data;
+        })
+        .catch(error => console.log(error));
+    }
+}
+</script>
